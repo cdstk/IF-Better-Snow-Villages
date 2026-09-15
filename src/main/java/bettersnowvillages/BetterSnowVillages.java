32b0f3711e5922efed16924f61c1ee8cf5259479
cdstk <162769767+cdstk@users.noreply.github.com>
2026-09-15 09:23:15 -0500

    1.0.0

--- src/main/java/bettersnowvillages/BetterSnowVillages.java
+++ src/main/java/bettersnowvillages/BetterSnowVillages.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class BetterSnowVillages {
     public static final String MODID = "bettersnowvillages";
-    public static final String VERSION = "0.0.0";
+    public static final String VERSION = "1.0.0";
     public static final String NAME = "Better Snow Villages";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
