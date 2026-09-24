aa12e20d504e79508609db1dbe385b95b9cabf00
cdstk <162769767+cdstk@users.noreply.github.com>
2026-09-24 00:40:26 -0500

    Bump 1.0.2

--- src/main/java/bettersnowvillages/BetterSnowVillages.java
+++ src/main/java/bettersnowvillages/BetterSnowVillages.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class BetterSnowVillages {
     public static final String MODID = "bettersnowvillages";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String NAME = "Better Snow Villages";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
